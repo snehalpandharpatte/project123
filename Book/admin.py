9fad3fcbce217378d40ca9f7fe4e4526d1ffbef7
snehalpandharpatte <81099219+snehalpandharpatte@users.noreply.github.com>
2021-04-24 12:07:15 +0530

    update admin.py with class student

--- Book/admin.py
+++ Book/admin.py
@@ -3,3 +3,14 @@ from Book.models import Book
 # Register your models here.
 
 admin.site.register(Book)
+
+
+
+class Student:
+  def __init__(self, name):
+    self.name = name
+    
+ 
+s1 = Student("ABC")
+s1 = Student("XYZ")
+s1 = Student("PQR")
