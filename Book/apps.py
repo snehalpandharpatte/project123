cd4db3d83e34281c42857de981b52a6e9124a825
snehalpandharpatte <snehal.pandharpatte@gmail.com>
2021-04-24 12:00:21 +0530

    added fourth commit

--- Book/apps.py
+++ Book/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class BookConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Book'
+
+
+
+
+def my_gen():
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+
